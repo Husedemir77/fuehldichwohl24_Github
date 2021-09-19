64a8e1b748a6e3152870584b6e580117f9b89dae
hüseyin özdemir <husedemirde@gmail.com>
2021-09-19 13:32:12 +0200

    Ver 2.2 - 19.09.2021

--- src/test/java/pages/Example1.java
+++ src/test/java/pages/Example1.java
@@ -1,6 +1,12 @@
 package pages;
 
-public class Example1 {
+import utilities.Driver;
+import utilities.ParentClass;
 
+public class Example1 extends ParentClass {
 
+    public void ex1() {
+        driver.manage().window().maximize();
+
+    }
 }
